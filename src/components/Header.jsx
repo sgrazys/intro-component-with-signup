c2c75415a2719926f9af7f1305cde163de243dc6
sgrazys <s.grazys@gmail.com>
2023-12-27 23:54:14 +0200

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+	return (
+		<div className='header'>
+			<h1 className='title'>Learn to code by watching others</h1>
+			<p className='content'>
+				See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.
+			</p>
+		</div>
+	);
+}
+
+export default Header;
