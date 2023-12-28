2087176c9b93a48d372a0503df8876701d3791a5
sgrazys <s.grazys@gmail.com>
2023-12-29 00:40:54 +0200

    pricing

--- src/components/PricingInfo.jsx
+++ src/components/PricingInfo.jsx
@@ -1,5 +1,11 @@
 function PricingInfo() {
-	return <p className='pricing'>PRICING INFO</p>;
+	return (
+		<div className='pricing-container'>
+			<p className='pricing-content'>
+				<strong>Try it free 7 days</strong> then $20/mo. thereafter
+			</p>
+		</div>
+	);
 }
 
 export default PricingInfo;
